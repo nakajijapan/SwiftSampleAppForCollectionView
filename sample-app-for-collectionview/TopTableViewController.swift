33662e851257e7535e4f83db4a20f2e8a8d6d0de
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2016-02-13 20:34:41 +0900

    add tableViewcontroller

---
+++ sample-app-for-collectionview/TopTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TopTableViewController.swift
+//  sample-app-for-collectionview
+//
+//  Created by nakajijapan on 2016/02/13.
+//  Copyright © 2016年 net.nakajijapan. All rights reserved.
+//
+
+import UIKit
+
+class TopTableViewController: UITableViewController {
+
+}
